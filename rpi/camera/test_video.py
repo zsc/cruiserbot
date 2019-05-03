e8cb85cd123d051a9f467abfb543335be995e801
Shuchang Zhou <shuchang.zhou@gmail.com>
2019-05-03 15:13:09 +0000

    Save video frames.

--- rpi/camera/test_video.py
+++ rpi/camera/test_video.py
@@ -1,4 +1,5 @@
 # import the necessary packages
+import numpy as np
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 import time
@@ -14,6 +15,7 @@ rawCapture = PiRGBArray(camera, size=(640, 480))
 time.sleep(0.1)
  
 # capture frames from the camera
+cnt = 0
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
     # grab the raw NumPy array representing the image, then initialize the timestamp
     # and occupied/unoccupied text
@@ -21,12 +23,16 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
  
     # show the frame
     print(image.sum(), 'image.sum', image.shape, 'image.shape')
+    np.save('image{:03d}.npy'.format(cnt), image)
     #cv2.imshow("Frame", image)
     #key = cv2.waitKey(1) & 0xFF
  
     # clear the stream in preparation for the next frame
     rawCapture.truncate(0)
  
+    cnt += 1
+    if cnt == 30:
+        break
     # if the `q` key was pressed, break from the loop
     #if key == ord("q"):
      #   break

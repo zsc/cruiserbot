701fc77017307ffe972ec11c6387c4a9e8690756
Shuchang Zhou <shuchang.zhou@gmail.com>
2019-05-04 00:41:26 +0000

    [pytorch] Use squeezenet.

--- pytorch/predict/pred_pc.py
+++ pytorch/predict/pred_pc.py
@@ -13,7 +13,7 @@ def get_loader():
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
 
 def get_model():
-    model = models.resnet18(pretrained=True)
+    model = models.squeezenet1_0(pretrained=True)
     #model = model_ft.to(device)
     model.eval()
     return model

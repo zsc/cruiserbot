701a5be3bcec9d19e965a45f52121d257ec46f87
Shuchang Zhou <shuchang.zhou@gmail.com>
2019-06-16 18:47:48 +0800

    [meowbit] Show bmp.

--- meowbit/micropython/main.py
+++ meowbit/micropython/main.py
@@ -1,9 +1,11 @@
 # main.py -- put your code here!
 import pyb
 import framebuf
-fbuf = bytearray(160*128)
-fb = framebuf.FrameBuffer(fbuf, 160, 128, framebuf.PL8)
+fbuf = bytearray(160*128*2)
+fb = framebuf.FrameBuffer(fbuf, 160, 128, framebuf.RGB565)
 tft = pyb.SCREEN()
-fb.fill(8)
-fb.text('Hello Micropython!', 10, 50, 2)
-tft.show(fb, 1)
+fb.fill(0)
+#fb.text('Hello Micropython!', 10, 50, 2)
+tft.show(fb)
+fb.loadbmp('nut.bmp')
+tft.show(fb)

47c71277983eadad7b3c28d9abbe7562b292fed2
Shuchang Zhou <shuchang.zhou@gmail.com>
2019-08-04 20:27:51 +0800

    Update script.

--- jetson_nano/utils/dump_video.py
+++ jetson_nano/utils/dump_video.py
@@ -1,13 +1,15 @@
+#!/usr/bin/env python3
 import cv2
 
-cap = cv2.VideoCapture('/dev/video1')
+cap = cv2.VideoCapture('/dev/video0')
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
 out = cv2.VideoWriter('output.avi',fourcc, 20.0, (640,480))
 try:
     while True:
         ret, img = cap.read()
-        print('#', end='', flush=True)
-        out.write(img)
+        if img is not None:
+            print('#', end='', flush=True)
+            out.write(img)
         #cv2.imshow('', img)
 finally:
     out.release()

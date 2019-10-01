9c1e07d1b05cac61699cb7d942c40619a95113c3
Shuchang Zhou <shuchang.zhou@gmail.com>
2019-10-01 16:46:51 +0800

    Control serial

--- flask/app/routes.py
+++ flask/app/routes.py
@@ -1,10 +1,12 @@
 import random
 import os
 import cv2
+import serial
 
 from flask import make_response
 from app import app
 
+ser = None
 cap = None
 
 @app.route('/')
@@ -28,17 +30,36 @@ def index():
         print(e)
         return "Hi there {}!".format(random.randint(0, 10))
 
+def proc_cmd(cmd):
+    ser_write = {'left':2, 'right':1, 'clean':3}[cmd]
+
+    global ser
+    if ser is None:
+        ser = serial.Serial('/dev/ttyACM0', 115200)
+    try:
+        if ser_write == 3:
+            ser.write(b'3\x0a\x0d')
+        elif ser_write == 2:
+            ser.write(b'2\x0a\x0d')
+        else:
+            ser.write(b'1\x0a\x0d')
+    except Exception as e:
+        print(e)
+
 @app.route('/can_recycle')
 def can_recycle():
-    print('can recycle')
+    proc_cmd('left')
+    print('left, can recycle')
     return ""
 
 @app.route('/cant_recycle')
 def cant_recycle():
-    print('can recycle')
+    proc_cmd('right')
+    print('right, can recycle')
     return ""
 
 @app.route('/clean')
 def clean():
+    proc_cmd('clean')
     print('clean')
     return ""

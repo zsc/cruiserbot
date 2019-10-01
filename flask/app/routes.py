15d8425a7af91137c5a409a426482b02a52109e7
Shuchang Zhou <shuchang.zhou@gmail.com>
2019-10-01 15:59:42 +0800

    Can get image.

--- flask/app/routes.py
+++ flask/app/routes.py
@@ -1,10 +1,32 @@
-from app import app
 import random
+import os
+import cv2
+
+from flask import make_response
+from app import app
+
+cap = None
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hi there {}!".format(random.randint(0, 10))
+    global cap
+    if cap is None:
+        dev = '/dev/video0'
+        if not os.path.exists(dev):
+            dev = 0
+        cap = cv2.VideoCapture(dev)
+
+    try:
+        ret, img = cap.read()
+        img = cv2.resize(img, (0, 0), fx=0.25, fy=0.25)
+        _, img_encoded = cv2.imencode('.jpg', img)
+        response = make_response(img_encoded.tostring())
+        response.headers.set('Content-Type', 'image/jpeg')
+        return response
+    except Exception as e:
+        print(e)
+        return "Hi there {}!".format(random.randint(0, 10))
 
 @app.route('/can_recycle')
 def can_recycle():

d2cdc4c98318cf61e3d7961f674ad670ae7147c9
Shuchang Zhou <shuchang.zhou@gmail.com>
2019-10-01 15:40:28 +0800

    Add hooks.

--- flask/app/routes.py
+++ flask/app/routes.py
@@ -5,3 +5,18 @@ import random
 @app.route('/index')
 def index():
     return "Hi there {}!".format(random.randint(0, 10))
+
+@app.route('/can_recycle')
+def can_recycle():
+    print('can recycle')
+    return ""
+
+@app.route('/cant_recycle')
+def cant_recycle():
+    print('can recycle')
+    return ""
+
+@app.route('/clean')
+def clean():
+    print('clean')
+    return ""

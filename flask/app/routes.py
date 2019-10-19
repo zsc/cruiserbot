3fb679578601b767bb46887b26a4a850ee105342
Shuchang Zhou <shuchang.zhou@gmail.com>
2019-10-19 09:47:24 +0800

    Add nearest neighbor classifier.

--- flask/app/routes.py
+++ flask/app/routes.py
@@ -11,6 +11,29 @@ ser = None
 cap = None
 image_pred = ImagePred()
 
+def load_dir(path='crops', test_ratio=0.3):
+    xs = []; ys = []; test_xs = []; test_ys = []
+    for label, tag in enumerate(['empty', 'nuts', 'pencil', 'tissue', 'plastic']):
+        for fname in glob.glob('{}/{}/*.png'.format(path, tag)):
+            img = cv2.imread(fname)
+            outputs = image_pred.pred(img)
+            #print(outputs.cpu().shape);break
+            feature = outputs.cpu().detach().numpy()[0]
+            if test_ratio > np.random.rand(1)[0]:
+                test_xs.append(feature); test_ys.append(label)
+            else:
+                xs.append(feature); ys.append(label)
+    return {'xs':xs, 'ys':ys, 'test_xs':test_xs, 'test_ys':test_ys}
+
+def build_classifier():
+    res = load_dir(test_ratio=0)
+    xs, ys = res['xs'], res['ys']
+    clf = neighbors.KNeighborsClassifier(n_neighbors, weights='distance')
+    clf.fit(xs, ys)
+    return clf
+
+classifier = build_classifier()
+
 @app.route('/')
 @app.route('/index')
 def index(cnt=[0]):
@@ -18,9 +41,10 @@ def index(cnt=[0]):
     cv2.imwrite('{:03d}.png'.format(cnt[0]), frame)
     cnt[0] += 1
     out = image_pred.pred(cv2.resize(frame, (224, 224))).cpu()
-    pred_res = image_pred.show_model_outputs(out, top_k=2, short=True)
+    pred_res = classifier.predict(out)
+    #pred_res = image_pred.show_model_outputs(out, top_k=2, short=True)
     print(pred_res)
-    return render_template('index.html', title=pred_res)
+    return render_template('index.html', title='{}'.format(pred_res))
 
 def gen():
     while True:

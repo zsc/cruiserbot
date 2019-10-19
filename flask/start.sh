f11ecfad95cc585e392e471e02ce4ecf4b584234
Shuchang Zhou <shuchang.zhou@gmail.com>
2019-10-19 23:28:12 +0800

    Fix startup script.

--- flask/start.sh
+++ flask/start.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-### BEGIN INIT INFO
-# Provides:             cruiserbot
-# Required-Start:       $remote_fs $syslog
-# Required-Stop:        $remote_fs $syslog
-# Default-Start:        2 3 4 5
-# Default-Stop:         
-# Short-Description:    OpenBSD Secure Shell server
-### END INIT INFO
-
 #[[ -x /dev/ttyACM0 ]] || sudo chmod 777 /dev/ttyACM0
-FLASK_APP=main.py flask run --host=0.0.0.0
+cd /home/zsc/cruiserbot/flask/
+FLASK_APP=/home/zsc/cruiserbot/flask/main.py flask run --host=0.0.0.0
